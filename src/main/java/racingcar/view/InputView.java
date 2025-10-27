44be68444995175f92f6799a28c968e637a21b3a
kolvv8 <sarrin1212@naver.com>
2025-10-27 22:40:44 +0900

    feat(view): create view package & add InputView

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readCarNames() {
+        return Console.readLine();
+    }
+
+    public String readTryCount() {
+        return Console.readLine();
+    }
+}
+
