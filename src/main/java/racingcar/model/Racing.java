4fcbed514851eb844c6eb017bb1cfa2bbdd6e623
kolvv8 <sarrin1212@naver.com>
2025-10-27 23:18:18 +0900

    feat(model): add movement logic & return status list

---
+++ src/main/java/racingcar/model/Racing.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    private final List<Car> cars;
+
+    public Racing(List<String> carNames) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : carNames) {
+            carList.add(new Car(name));
+        }
+        this.cars = carList;
+    }
+
+    public void playRound() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<String> getCurrentStatus() {
+        List<String> statuses = new ArrayList<>();
+        for (Car car : cars) {
+            statuses.add(car.getCurrentStatusString());
+        }
+        return statuses;
+    }
+
+}
